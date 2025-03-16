19f585c1e0b0b57c5a385b83a19eeb8a61f678b3
izaan <izaansaquib@gmail.com>
2025-03-17 00:25:11 +0500

    feat: add login and write pages

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -7,7 +7,7 @@ import { useState } from "react";
 const AuthLinks = () => {
   const [open, setOpen] = useState(false);
   // ? temporary
-  const status = "unauthenticated";
+  const status = "authenticated";
   return (
     <>
       {
